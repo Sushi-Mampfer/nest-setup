ad0e912a9607401b45a06cfb53bc7e37cf6f598c
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2025-09-05 11:40:33 +0200

    chatgpt worked, just some qol

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ if [ -z "$name" ]; then
   name="service"
 fi
 
-curl -L https://github.com/Sushi-Mampfer/nest-setup/releases/latest/download/nest-service \
-  --create-dirs -o "$HOME/.local/bin/$name"
+curl -sSfL https://github.com/Sushi-Mampfer/nest-setup/releases/latest/download/nest-service --create-dirs -o "$HOME/.local/bin/$name"
 
 chmod +x "$HOME/.local/bin/$name"
 
