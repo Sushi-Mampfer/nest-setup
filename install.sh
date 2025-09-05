abdad1d6a209584b33b85d763e3230598e4ad0cb
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2025-09-05 11:47:39 +0200

    bug fix

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ -z "$name" ]; then
   name="service"
 fi
 
-curl -sSfL https://github.com/Sushi-Mampfer/nest-setup/releases/latest/download/nest-service --create-dirs -o "$HOME/.local/bin/$name"
+curl -sSfL https://github.com/Sushi-Mampfer/nest-setup/releases/latest/download/nest-setup --create-dirs -o "$HOME/.local/bin/$name"
 
 chmod +x "$HOME/.local/bin/$name"
 
